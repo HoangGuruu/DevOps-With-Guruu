799ffcedce6a23fe9bf21e7f1e11c80fd0d8c24c
jenkins <nthoangthcs@gmail.com>
2024-02-19 22:13:18 +0700

    c3

---
+++ Chapter3/README.md
@@ -0,0 +1,4 @@
+# Install Docker
+
+# Command docker 
+
---
+++ Chapter3/app/app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, Docker with Flask!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
